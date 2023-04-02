903d2005b30d24e29b67d1532164fc01813a5740
Afzaal Ahmad <ahmadafzaal702@gmail.com>
2023-04-02 20:00:22 +0500

    complete

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 
 const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="max-w-screen-xl mx-auto px-16"> {children}</div>;
+  return (
+    <div className="max-w-screen-xl mx-auto px-10 sm:px-12"> {children}</div>
+  );
 };
 
 export default Wrapper;
