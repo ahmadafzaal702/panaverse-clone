ee8a437f74b6a63352e79852cb9f974d7c375d53
Afzaal Ahmad <ahmadafzaal702@gmail.com>
2023-04-02 19:39:01 +0500

    complete app

--- src/components/widgets/DataSpecializedTracks.tsx
+++ src/components/widgets/DataSpecializedTracks.tsx
@@ -90,6 +90,27 @@ export const DataSpecializedTracks = [
     ],
   },
   {
+    slug: "wmd",
+    header: "Web 3.0 (Blockchain) and Metaverse Specialization",
+    image: Metaverse,
+    description:
+      "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
+    quarters: [
+      {
+        header: "Quarter IV",
+        description:
+          "W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps",
+        number: "4",
+      },
+      {
+        header: "Quarter V",
+        description:
+          "MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences",
+        number: "5",
+      },
+    ],
+  },
+  {
     slug: "ai",
     header: "Artificial Intelligence (AI) and Deep Learning Specialization",
     image: AI,
@@ -105,6 +126,49 @@ export const DataSpecializedTracks = [
       {
         header: "Quarter V",
         description: "AI-361: Deep Learning and MLOps",
+        number: "5",
+      },
+    ],
+  },
+
+  {
+    slug: "cc",
+    header: "Cloud-Native Computing Specialization",
+    image: Cloud,
+    description:
+      "The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.",
+    quarters: [
+      {
+        header: "Quarter IV",
+        description:
+          "CN-351: Certified Kubernetes Application Developer (CKAD)",
+        number: "4",
+      },
+      {
+        header: "Quarter V",
+        description:
+          "CN-361: Developing Multi-Cloud Apps using CDK for Terraform",
+        number: "5",
+      },
+    ],
+  },
+
+  {
+    slug: "ac",
+    header: "Ambient Computing and IoT Specialization",
+    image: Ambient,
+    description:
+      "The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices.",
+    quarters: [
+      {
+        header: "Quarter IV",
+        description:
+          "AC-351: Ambient Computing with Voice Assistants and Matter Devices",
+        number: "4",
+      },
+      {
+        header: "Quarter V",
+        description: "AC-361: Embedded Programming using C and Rust",
         number: "5",
       },
     ],
